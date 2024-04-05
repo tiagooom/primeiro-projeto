2b31325e839ef0cdda8bc91767660435d2d9498f
Tiago <tiagooom88@gmail.com>
2024-04-05 10:40:55 -0200

    finalizado com entrada e retorno

--- arrayPerm/index.php
+++ arrayPerm/index.php
@@ -1,1 +1,21 @@
 <?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function buildArray($nums) {        
+        foreach ($nums as $k => $v) {
+            $arr[$k] = $nums[$v];            
+        }
+        return $arr;
+    }
+}
+
+$entrada = [0,2,1,5,3,4];
+
+$resposta = new Solution();
+$r = $resposta->buildArray($entrada);
+var_dump($r);
