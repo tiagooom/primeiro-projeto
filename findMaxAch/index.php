153d7356e81bfef2ad7600f1eb2e438e65cf13dc
Tiago <tiagooom88@gmail.com>
2024-04-05 09:41:36 -0200

    iniciando desafio

---
+++ findMaxAch/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @param Integer $t
+     * @return Integer
+     */
+    function theMaximumAchievableX($num, $t) {
+        $x = 0;
+
+        return $x;
+    }
+}
+
+$entrada = 4;
+$t = 1;
+
+$resposta = new Solution();
+$r = $resposta->theMaximumAchievableX($entrada, $t);
+var_dump($x);
